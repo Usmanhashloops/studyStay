4f73065edc342d41352b13686a58b4b29f37180e
usamaajaved <usama-javed@hotmail.com>
2023-12-22 17:13:05 +0500

    merge code

--- src/utils/Url.js
+++ src/utils/Url.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://study-stay.hashloops.com/backend/public/api/";
-export const IMAGE_BASE_URL = "http://study-stay.hashloops.com/backend/public/images/";
+export const API_BASE_URL = "https://study-stay.hashloops.com/backend/public/api/";
+export const IMAGE_BASE_URL = "https://study-stay.hashloops.com/backend/public/images/";
 // export const API_BASE_URL = "http://192.168.100.2:8000/api/";
 // export const IMAGE_BASE_URL = "http://192.168.100.2:8000/images/";
